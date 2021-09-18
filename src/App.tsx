bf4c7deb1e6f465e0d7d3f2be94dcf4dab671f03
Niv Zelber <nivznivz0@gamil.com>
2021-09-19 00:12:49 +0300

    clean project and add readme

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,97 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import produce from "immer";
+import React, { useCallback, useRef, useState } from "react";
+
+const rowCount = 40;
+const colCount = 95;
+const interval = 100;
+
+const cases = [
+  [0, 1],
+  [0, -1],
+  [1, 0],
+  [1, -1],
+  [1, 1],
+  [-1, 0],
+  [-1, 1],
+  [-1, -1]
+];
 
 function App() {
+  const [grid, setGrid] = useState<number[][]>(() =>
+    new Array(rowCount).fill(new Array(colCount).fill(0))
+  );
+
+  const [running, setRunning] = useState(false);
+
+  const runningRef = useRef(running);
+  runningRef.current = running;
+
+  const start = useCallback(() => {
+    if (!runningRef.current) {
+      return;
+    }
+
+    setGrid(g =>
+      produce(g, gridCopy => {
+        g.forEach((row, i) =>
+          row.forEach((col, j) => {
+            let neighbors = 0;
+            cases.forEach(([x, y]) => {
+              const newX = x + i;
+              const newY = y + j;
+              if (newX >= 0 && newX < rowCount && newY >= 0 && newY < colCount) {
+                neighbors += g[newX][newY];
+              }
+            });
+
+            if (neighbors < 2 || neighbors > 3) {
+              gridCopy[i][j] = 0;
+            } else if (g[i][j] === 0 && neighbors === 3) {
+              gridCopy[i][j] = 1;
+            }
+          })
+        );
+      })
+    );
+
+    setTimeout(start, interval);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button
+        onClick={() => {
+          setRunning(!running);
+          if (!running) {
+            runningRef.current = true;
+            start();
+          }
+        }}
+      >
+        {running ? "Stop" : "Start"}
+      </button>
+      <div style={{ display: "grid", gridTemplateColumns: `repeat(${colCount}, 20px)` }}>
+        {grid.map((row, i) =>
+          row.map((col, j) => (
+            <div
+              key={`${i} - ${j}`}
+              style={{
+                height: 20,
+                width: 20,
+                border: "1px solid black",
+                backgroundColor: col === 0 ? "white" : "pink"
+              }}
+              onClick={() => {
+                const newGrid = produce(grid, gridCopy => {
+                  gridCopy[i][j] = gridCopy[i][j] === 0 ? 1 : 0;
+                });
+                setGrid(newGrid);
+              }}
+            ></div>
+          ))
+        )}
+      </div>
+    </>
   );
 }
 
